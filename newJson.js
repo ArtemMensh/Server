db158d74dc337f4d4a378ce2e0c449f523cf024f
Menshakov Artem <artem.menshakov.1999@mail.ru>
2019-12-30 15:10:24 +0300

    fixed structure newMedicament.json. Deleted all records where ChemistryName = ~

--- newJson.js
+++ newJson.js
@@ -2,19 +2,42 @@ var data = require("./Medicaments.json")
 fs=require("fs")
 
 
-var newData = {}
+newMedicament()
+testJSON()
 
-for(var i = 0; i<data.length; i++)
-{
-	name = data[i].TradeName
-	try{
-		newData[name][newData[name].length] = data[i]
+
+function newMedicament(){
+	var count = 0 
+	var newData = {}
+
+	for(var i = 0; i<data.length; i++)
+	{
+		if(data[i].ChemistryName == '~') continue 
+		name = data[i].TradeName
+		try{
+			newData[name][newData[name].length] = data[i]
+		}
+		catch(err){
+			newData[name] = []
+			newData[name][newData[name].length] = data[i]
+		}
+		count++
 	}
-	catch(err){
-		newData[name] = []
-		newData[name][newData[name].length] = data[i]
-	}
-	count++
+
+	fs.writeFileSync("newMedicaments.json", JSON.stringify(newData, null, 2))
 }
 
-fs.writeFileSync("newMedicaments.json", JSON.stringify(newData))
+function testJSON() {
+
+	testJSON = []
+
+	for(var i = 0; i<5; i++)
+	{
+		if(data[i].ChemistryName == '~') continue
+		testJSON[i] = data[i]
+		
+	}
+
+	fs.writeFileSync("testJSON.json", JSON.stringify(testJSON, null, 2))
+
+}
